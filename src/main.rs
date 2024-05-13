8e0924e7ccf489f70db0ff8e4f26dae8119ce423
JonasRex <78900583+JonasRex@users.noreply.github.com>
2024-05-13 15:15:51 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,56 +19,7 @@ fn main() {
         Box::new(|_cc| Box::new(MyApp::new())));
 }
 
-// #[derive(Default, Clone)]
-// struct MyApp {
-//     pages: PathBuf,
-//     start_dir: String
-// }
 
-// impl MyApp {
-//     fn new() -> Self {
-//         let mut app = MyApp {
-//             pages: PathBuf::new(),
-//             start_dir: String::from("test-directory")
-//         };
-//         app.initialize();
-//         app
-//     }
-//
-//     fn initialize(&mut self) {
-//         self.pages = PathBuf::from(&self.start_dir);
-//     }
-//
-//     fn add_page(&mut self, folder_name: &str){
-//         self.pages.push(folder_name);
-//     }
-// }
-//
-// impl eframe::App for MyApp {
-//     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-//         egui::CentralPanel::default().show(ctx, |ui| {
-//
-//             ui.heading(RichText::new(utils::get_clean_abs_path(self.pages.to_str().unwrap()).to_str().unwrap()).size(13.0));
-//
-//             let self_clone = self.clone();
-//             let mut counter = 0;
-//             let screen_size = ctx.available_rect();
-//             let mut path = PathBuf::new();
-//
-//             egui::ScrollArea::horizontal().show(ui, |ui| {
-//                 ui.horizontal(|ui| {
-//                     for page in self_clone.pages.components() {
-//                         let curr_folder_name = page.as_os_str().to_str().unwrap();
-//                         path.push(curr_folder_name);
-//
-//                         ui_folders(ui, self, &counter, path.to_str().unwrap(), &screen_size.height());
-//                         counter += 1;
-//                     }
-//                 });
-//             });
-//         });
-//     }
-// }
 
 fn ui_folders(ui: &mut egui::Ui, _self: &mut MyApp, index: &i32, curr_path: &str, screen_height: &f32) {
     ui.vertical(|ui| {
