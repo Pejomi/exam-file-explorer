ff7ff7848c02cdf1a91a5ccbf1fb84fde588fbc9
MieThorberg <cph-mt373@cphbusiness.dk>
2024-05-13 14:53:51 +0200

    search input field added

--- src/app.rs
+++ src/app.rs
@@ -5,13 +5,15 @@ use egui_extras::{TableBuilder, Column};
 use egui_extras::{Size, StripBuilder};
 #[derive(Default)]
 pub(crate) struct MyApp {
-    pages: Vec<FolderContents>
+    pages: Vec<FolderContents>,
+    search_query: String
 }
 
 impl MyApp {
     pub(crate) fn new() -> Self {
         let mut app = MyApp {
-            pages: Vec::new()
+            pages: Vec::new(),
+            search_query: String::new()
         };
         app.initialize();
         app
@@ -42,6 +44,12 @@ impl eframe::App for MyApp {
                                     ui.button("⟳");
                                     // todo: show current path (input field)
                                     // todo: add search (input field)
+                                    ui.add(egui::TextEdit::singleline(&mut self.search_query).desired_width(100.0));
+                                    if ui.button("Search").clicked() {
+                                        // Trigger search action here
+                                        //search_by_name(&app.root_path, &app.search_query);
+                                        println!("Search query: {}", self.search_query)
+                                    }
                                 });
                             });
                         });
@@ -66,74 +74,86 @@ impl eframe::App for MyApp {
 
                     // body
                     strip.cell(|ui| {
-                        egui::SidePanel::left("left_panel")
-                            .resizable(true)
-                            .default_width(100.0)
-                            .width_range(100.0..=500.0)
-                            .show_inside(ui, |ui| {
-                                ui.vertical(|ui| {
-                                egui::ScrollArea::vertical().show(ui, |ui| {
-                                    // directory tree // todo: replace dummy text
-                                    CollapsingHeader::new("Home")
-                                        .id_source("1")
-                                        .default_open(false)
-                                        .show(ui, |ui| {
-                                            ui.label("hello hello hello hello hello hello");
-                                            ui.label("hello");
-                                            CollapsingHeader::new("Home")
-                                                .default_open(false)
-                                                .show(ui, |ui| {
-                                                    ui.label("hello");
-                                                    ui.label("hello");
-                                                    ui.label("hello")
-                                                })
-                                                .body_returned;
-                                        })
-                                        .body_returned;
-                                    });
-                                });
-                            });
+                        // egui::SidePanel::left("left_panel")
+                        //     .resizable(true)
+                        //     .default_width(100.0)
+                        //     .width_range(100.0..=500.0)
+                        //     .show_inside(ui, |ui| {
+                        //         ui.vertical(|ui| {
+                        //         egui::ScrollArea::vertical().show(ui, |ui| {
+                        //             // directory tree // todo: replace dummy text
+                        //             CollapsingHeader::new("Home")
+                        //                 .id_source("1")
+                        //                 .default_open(false)
+                        //                 .show(ui, |ui| {
+                        //                     ui.label("hello hello hello hello hello hello");
+                        //                     ui.label("hello");
+                        //                     CollapsingHeader::new("Home")
+                        //                         .default_open(false)
+                        //                         .show(ui, |ui| {
+                        //                             ui.label("hello");
+                        //                             ui.label("hello");
+                        //                             ui.label("hello")
+                        //                         })
+                        //                         .body_returned;
+                        //                 })
+                        //                 .body_returned;
+                        //             });
+                        //         });
+                        //     });
 
                         egui::CentralPanel::default().show_inside(ui, |ui| {
                             egui::ScrollArea::vertical().show(ui, |ui| {
                                 // table display list
-                                TableBuilder::new(ui)
-                                    //.striped(true)
-                                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                                    .column(Column::remainder().resizable(true))
-                                    .column(Column::remainder().resizable(true))
-                                    .column(Column::remainder().resizable(true))
-                                    .column(Column::remainder().resizable(true))
-                                    .header(20.0, |mut header| {
-                                        header.col(|ui| {
-                                            ui.label("Name");
-                                        });
-                                        header.col(|ui| {
-                                            ui.label("Date modified");
-                                        });
-                                        header.col(|ui| {
-                                            ui.label("Type");
-                                        });
-                                        header.col(|ui| {
-                                            ui.label("Size");
-                                        });
-                                    })
-                                    .body(|mut body| { // todo: loop and display files from current path
-                                        body.row(30.0, |mut row| {
-                                            row.col(|ui| {
-                                                ui.label("Hello");
-                                            });
-                                            row.col(|ui| {
-                                                ui.label("Hello");
-                                            });
-                                            row.col(|ui| {
-                                                ui.label("Hello");
-                                            });
-                                            row.col(|ui| {
-                                                ui.button("world!");
-                                            });
-                                        });
+                                // TableBuilder::new(ui)
+                                //     //.striped(true)
+                                //     .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                                //     .column(Column::remainder().resizable(true))
+                                //     .column(Column::remainder().resizable(true))
+                                //     .column(Column::remainder().resizable(true))
+                                //     .column(Column::remainder().resizable(true))
+                                //     .header(20.0, |mut header| {
+                                //         header.col(|ui| {
+                                //             ui.label("Name");
+                                //         });
+                                //         header.col(|ui| {
+                                //             ui.label("Date modified");
+                                //         });
+                                //         header.col(|ui| {
+                                //             ui.label("Type");
+                                //         });
+                                //         header.col(|ui| {
+                                //             ui.label("Size");
+                                //         });
+                                //     })
+                                //     .body(|mut body| { // todo: loop and display files from current path
+                                //         body.row(30.0, |mut row| {
+                                //             row.col(|ui| {
+                                //                 ui.label("Hello");
+                                //             });
+                                //             row.col(|ui| {
+                                //                 ui.label("Hello");
+                                //             });
+                                //             row.col(|ui| {
+                                //                 ui.label("Hello");
+                                //             });
+                                //             row.col(|ui| {
+                                //                 ui.button("world!");
+                                //             });
+                                //         });
+                                //     });
+                                //directory list
+                                let pages_clone = self.pages.clone();
+                                let mut counter = 1;
+                                let screen_size = ui.max_rect();
+                                egui::ScrollArea::horizontal().show(ui, |ui| {
+                                    ui.horizontal(|ui| {
+                                        for page in pages_clone {
+                                            ui_folders(ui, &page.items, &mut self.pages, &counter, &screen_size.height());
+                                            counter += 1;
+                                        }
                                     });
+                                });
                             });
                         });
                         //directory list
